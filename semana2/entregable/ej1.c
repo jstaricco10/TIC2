3def3322ba7e368f4cd6c49cec663ec8d1e83878
jstaricco10 <jstaricco@correo.um.edu.uy>
2020-03-19 21:21:07 -0300

    entre

--- semana2/entregable/ej1.c
+++ semana2/entregable/ej1.c
@@ -38,6 +38,7 @@ main(){
 
 
 	// Impresion horizontal
+    printf("\nHistograma horizontal de palabras por longitud. \n\n");
 	for (i = 0; i <10;i++)
     	{
      	printf("%d: ",i+1);
@@ -45,6 +46,32 @@ main(){
         putchar('X');
     	putchar('\n');
     } 
+
+    // Impresion vertical 
+    printf("\nHistograma vertical de palabras por longitud. \n\n");
+    int masCant = 0;
+    for (i = 0; i<11; i++)
+        if(largos[i] > masCant)
+            masCant = largos[i];
+    for (i = masCant ; i > 0; i--){
+        printf("%2s  ", ">" );
+        for (j = 0; j < 11 ; j++){
+            if (largos[j] >= i)
+                printf("%2s ","X");
+            else
+                printf("%2s "," ");
+        }
+        printf("\n");        
+    }
+    for (i=0 ; i < 11;i++)
+        printf("====");
+    printf("\n");
+    printf("%2s  ", " ");
+    for (int i = 0; i < 10; ++i)
+    {
+        printf("%2d ",i+1);
+    }
+
 
 
 
