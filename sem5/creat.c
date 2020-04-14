e74316e53734931727043907ef250f23d0604191
STARICCO RODRÍGUEZ <jstaricco@VM-FIUM-SO.agt1aeotuneepp23yebz0qxtbb.cx.internal.cloudapp.net>
2020-04-14 17:29:23 -0300

    sss

--- sem5/creat.c
+++ sem5/creat.c
@@ -16,14 +16,14 @@ int open(const char *pathname, int flags, mode_t mode);
 int main(int argc, char const *argv[])
 {
 	int fd; 	 //descriptor del archivo
-	int flags = O_CREAT | O_EXCL | O_WRONLY | O_TRUNC; //man creat o man open
-	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH; //0644 - rw para duenio, r para grupo y otros
+	int flags = O_CREAT|O_EXCL|O_WRONLY|O_TRUNC; //man creat o man open
+	mode_t mode = S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH; //0644 - rw para duenio, r para grupo y otros
 
 	while(--argc > 0){
-		if((fd = open(++argv,flags,mode)) < 0){
+		if((fd = open(*++argv,flags,mode)) < 0){
 			error (0, errno, "No se pudo crear el archivo %s", *argv);
 		} else {
-			fprintf(stderr, "archivo %s creado, descriptor asignado %d. \n", *argv, fd);
+			fprintf(stderr, "archivo %s creado, descriptor asignado %d. \n", *argv ,fd);
 		}
 	}
 	exit(0);
