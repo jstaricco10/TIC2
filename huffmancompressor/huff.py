e7ea027b60f672a2dfa7a3aa7ed1820357bfd1ef
jstaricco10 <jstaricco@correo.um.edu.uy>
2020-06-19 16:23:49 -0300

    avance

--- huffmancompressor/huff.py
+++ huffmancompressor/huff.py
@@ -4,25 +4,19 @@ from heapq import heappush, heappop, heapify
 from collections import defaultdict, namedtuple
 import argparse
 import os
+import struct
 
 # traducir huff a named tuple huff(symbol,code), named tuple no porque una tupla no permite asignacion, debe ser un dict
 
-"""
-typedef struct compr_header: # 8 bytes: cabezal de archivo comprimido
-    uint16_t    magic_nbr # número mágico (2)
-    uint8_t     sym_arraylen # largo del array de los Symcode (1)
-    uint8_t     sym_arraysize #  tamaño de cada elemento del array anterior (1)
-    uint32_t    filelen # largo del archivo original en bytes (4)
-} Compr_header
-"""
-
 """ Se debe hacer una clase para el Header?"""
+
+
 class Header:
     def __init__(self, magic_nbr, sym_arraylen, sym_arraysize, filelen):
-        self.magic_nbr = magic_nbr
-        self.sym_arraylen = sym_arraylen
-        self.sym_arraysize = sym_arraysize
-        self.filelen = filelen
+        self.magic_nbr = magic_nbr  # 2 bytes
+        self.sym_arraylen = sym_arraylen  # 1 byte
+        self.sym_arraysize = sym_arraysize  # 1 byte
+        self.filelen = filelen  # 1 byte
 
 
 def encode(symb2freq):
@@ -49,6 +43,7 @@ def encode(symb2freq):
             pop = elem
             lista.append(huffCode(pop[0], pop[1]))
     # ordeno por codigo??, con item y atr getter de la letra
+    print(lista)
     return lista
 
 
@@ -58,7 +53,7 @@ def encode(symb2freq):
 def compress(huff, args):
     file = open(args.file, 'rb')
     # print(huff)
-    newfile = open(args.file + ".huff", 'wb')
+
     # primero debemos hacer el cabezal
     numeromagico = "DG"
     sym_arraylen = len(huff)
@@ -79,10 +74,22 @@ def compress(huff, args):
     cantAAgregar = 8 - (len(codificadoTotal) % 8)
     for _ in range(cantAAgregar):
         codificadoTotal += '0'
-    if len(codificadoTotal)/8 > filelen and not args.force:
+    if len(codificadoTotal) / 8 > filelen and not args.force:
         file.close()
         return
-    newfile.write(bytearray(int(codificadoTotal[x:x + 8], 2) for x in range(0, len(codificadoTotal), 8)))
+    # comentado para trabajar mientras con archivos chicos sin -f
+    # if not args.force:
+    #     if filelen < len(codificadoTotal):
+    #         print("El archivo resultante comprimido es mas grande que el dado.")
+    #         file.close()
+    #         return
+    newfile = open(args.file + ".huff", 'wb')
+    hd = Header('JA', len(huff), len(huff[-1].code), filelen)
+    for x in range(2):  # 4 por ser el largo en bytes del hd
+        newfile.write(struct.pack('!I', int(hd[x: x + 8], 2)))
+    for x in range(0, len(codificadoTotal), 8):
+        newfile.write(struct.pack('!I', int(codificadoTotal[x: x + 8], 2)))
+    # newfile.write(bytearray(int(codificadoTotal[x:x + 8], 2) for x in range(0, len(codificadoTotal), 8)))
     # analizar, splitea el string en "8 char
     # chunks https://stackoverflow.com/questions/7290943/write-a-string-of-1s-and-0s-to-a-binary-file
     newfile.close()
@@ -93,7 +100,7 @@ def compress(huff, args):
 def main():
     parser = argparse.ArgumentParser(description='Parsea los datos pasados en consola')
     parser.add_argument('file', help='Archivo a comprimir')
-    parser.add_argument('-f', '--force', help='Fuerza compresion aunque aumente el tamano', type=str.upper)
+    parser.add_argument('-f', '--force', help='Fuerza compresion aunque aumente el tamano', action='store_true')
     parser.add_argument('-v', '--verbose', help='Imprime informacion del avance del proceso', action='store_true')
     # con el parser debo procesar el nombre del archivo
     args = parser.parse_args()
@@ -110,9 +117,9 @@ def main():
         print("Symbol\tWeight\tHuffman Code")
         for p in huff:
             print("%s\t%s\t%s" % (p.symbol, symb2freq[p.symbol], p.code))
-    # print(huff)
     compress(huff, args)
 
 
 if __name__ == '__main__':
     main()
+    os._exit(0)
