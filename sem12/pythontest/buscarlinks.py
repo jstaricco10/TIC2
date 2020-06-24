a3ffa4afb2023bbec93db29a4f39d9d8479ee5cf
jstaricco10 <jstaricco@correo.um.edu.uy>
2020-06-24 13:56:31 -0300

    ej 2

--- sem12/pythontest/buscarlinks.py
+++ sem12/pythontest/buscarlinks.py
@@ -2,18 +2,33 @@
 import argparse
 import sys
 import os
+from collections import namedtuple
 
 
 def main():
     parser = argparse.ArgumentParser(description='Parsea los datos del archivo pasado en consola')
-    parser.add_argument('dir', help='directorio al cual le analizamos los inode')
+    parser.add_argument('dir', help='directorio al cual le analizamos los inode, se debe pasar path relativo')
     args = parser.parse_args()
     path = args.dir
 
+    fileTuple = namedtuple('fileTuple', 'name inode')
     dir = os.scandir(path)
     print(path)
+    list = []
+    links = {}
     for elem in dir:
-        print(elem.name, " :", elem.inode())
+        print(elem.name, " :", os.stat(elem.name).st_ino)
+        list.append(fileTuple(elem.name, os.stat(elem.name).st_ino))
+    # Restaria ver cuales son links y ponerlos en pantalla con el inode al que apuntan
+    for e in list:
+        links[e.inode] = []
+    for e in list:
+        links[e.inode].append(e.name)
+    print("Los inodes presentes con sus respectivos files son:")
+    for e in links:
+        print(e)
+        print(links[e])
+        print()
     pass
 
 
